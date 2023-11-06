e99e883334e45a035314028d22239edf9e0f4432
anzmax <anzmaksimovic@gmail.com>
2023-11-06 18:54:48 +0300

    added categories and notification center

--- ChuckNorris-Example/Controllers/Categories/DetailVC.swift
+++ ChuckNorris-Example/Controllers/Categories/DetailVC.swift
@@ -1,23 +1,8 @@
 import UIKit
-import RealmSwift
 
-class QuoteVC: UIViewController {
+class DetailVC: UIViewController {
     
-    var currentQuote: Quote?
-    var lastLoadedQuote: Quote?
     var quotes: [Quote] = []
-    var service: FetchService
-    var storageService: StorageService
-    
-    init(sevice: FetchService, storageService: StorageService) {
-        self.service = sevice
-        self.storageService = storageService
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -25,20 +10,18 @@ class QuoteVC: UIViewController {
         tableView.backgroundColor = .white
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(QuoteCell.self, forCellReuseIdentifier: QuoteCell.id)
+        tableView.register(DetailCell.self, forCellReuseIdentifier: DetailCell.id)
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         setupViews()
         setupConstraints()
-        currentQuote = lastLoadedQuote
     }
     
     func setupViews() {
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Download", style: .plain, target: self, action: #selector(barButtonTapped))
+        view.backgroundColor = .white
         view.addSubview(tableView)
     }
     
@@ -50,49 +33,24 @@ class QuoteVC: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
-    
-    @objc func barButtonTapped() {
-        loadQuote()
-    }
-    
-    func loadQuote() {
-        service.fetchQuote { result in
-            switch result {
-            case .success(let quote):
-                self.lastLoadedQuote = quote
-                
-                self.storageService.append(quote)
-                print(self.storageService.load())
-                
-                self.currentQuote = quote
-                self.tableView.reloadData()
-                
-                NotificationCenter.default.post(name: NSNotification.Name("NewQuoteAdded"), object: nil)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
 }
 
-extension QuoteVC: UITableViewDelegate, UITableViewDataSource {
+extension DetailVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return quotes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let currentQuote = currentQuote else {
-            return UITableViewCell()
-        }
-        let cell = tableView.dequeueReusableCell(withIdentifier: QuoteCell.id, for: indexPath) as! QuoteCell
-        cell.update(with: currentQuote)
+        let cell = tableView.dequeueReusableCell(withIdentifier: DetailCell.id, for: indexPath) as! DetailCell
+        let quote = quotes[indexPath.row]
+        cell.update(with: quote)
         return cell
     }
 }
 
-class QuoteCell: UITableViewCell {
+class DetailCell: UITableViewCell {
     
-    static let id = "QuoteCell"
+    static let id = "DetailCell"
     
     var quoteLabel: UILabel = {
         let label = UILabel()
